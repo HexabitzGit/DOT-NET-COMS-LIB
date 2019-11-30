dbab7d573525ed3a91e6f96e52ffa1693e62fbf6
hani@hexabitz.com <hani@hexabitz.com>
2019-11-30 15:48:09 +0200

    Added the led and load cell message codes

--- DOT-NET-COMS-LIB/HexaInterface.cs
+++ DOT-NET-COMS-LIB/HexaInterface.cs
@@ -17,49 +17,15 @@ namespace DOT_NET_COMS_LIB
         public string Opt2_16_BIT_Code = "1";
         public string Opt1_Extended_Flag = "0";
         public byte[] AllMessage;
+
         public HexaInterface(string COM)
         {
             this.COM = COM;
         }
 
-        public void Start()
-        {
-            Port = new SerialPort("COM" + COM, 921600, Parity.None, 8, StopBits.One); // The default values to be used in the connection.
-            try { Port.Open(); } catch { }
-        }
-
-        public void End()
-        {
-            try { Port.Close(); } catch { }
-            Port.Dispose();
-        }
-
-        //public byte Options (Options1_Extended_Flag options1_Extended_Flag,
-        //                        Options2_16_BIT_Code options2_16_BIT_Code,
-        //                        Options34_Trace_Options options34_Trace_Options,
-        //                        Options5_Reserved options5_Reserved,
-        //                        Options67_Response_Options options67_Response_Options,
-        //                        Options8_Next_Message options8_Next_Message)  // 00100010 // 0x22
-        //{
-        //    string optionsString = "" + options1_Extended_Flag +
-        //                                options2_16_BIT_Code +
-        //                                options34_Trace_Options +
-        //                                options5_Reserved +
-        //                                options67_Response_Options +
-        //                                options8_Next_Message;
-
-        //    char[] charArray = optionsString.ToCharArray();
-        //    Array.Reverse(charArray);
-        //    optionsString = new string(charArray);
-        //    return byte.Parse(optionsString);
-        //}
-
         // Method to send the buffer to Hexabitz modules.
-
-
-        public void SendMessage(byte Destination, byte Source, int Code, byte[] Payload) 
+        public void SendMessage(byte Destination, byte Source, int Code, byte[] Payload)
         {
-
             string optionsString = Opt8_Next_Message +
                                    Opt67_Response_Options +
                                    Opt5_Reserved +
@@ -71,13 +37,22 @@ namespace DOT_NET_COMS_LIB
             Start();
             Message _Message = new Message(Destination, Source, Options, Code, Payload);
             AllMessage = _Message.GetAll();  // We get the whole buffer bytes to be sent to the Hexabitz modules.
-            
-            try { Port.Open(); } catch { }
-            try { Port.Write(AllMessage, 0, AllMessage.Length); } catch { Console.WriteLine("Connection Error"); }
-            try { Port.Close(); } catch { }
+            try { Port.Write(AllMessage, 0, AllMessage.Length); } catch (Exception exp) { Console.WriteLine("Connection Error"); }
             End();
         }
 
+        public void Start()
+        {
+            Port = new SerialPort("COM" + COM, 921600, Parity.None, 8, StopBits.One); // The default values to be used in the connection.
+            try { Port.Open(); } catch (Exception exp) { }
+        }
+
+        public void End()
+        {
+            try { Port.Close(); } catch (Exception exp) { }
+            Port.Dispose();
+        }
+        
         // The reviceing method to listen to the port if we got any response from it.
         private void Receive()
         {
@@ -133,64 +108,76 @@ namespace DOT_NET_COMS_LIB
         // Enum for the codes to be sent to the modules
         public enum Message_Codes
         {
-            // H26R0x
-            CODE_H26R0_STREAM_PORT_GRAM = 1901,
+            // H01R0 (Led Module)
+            CODE_H01R0_ON			     =	100,
+            CODE_H01R0_OFF			     =	101,
+            CODE_H01R0_TOGGLE		     =	102,
+            CODE_H01R0_COLOR		     =	103,
+            CODE_H01R0_PULSE	       	 =	104,
+            CODE_H01R0_SWEEP		     =	105,
+            CODE_H01R0_DIM			     =	106,
+
+            // H0FR6x
+            CODE_H0FR6_ON		         =	750,
+            CODE_H0FR6_OFF			     =	751,
+            CODE_H0FR6_TOGGLE		     =	752,
+            CODE_H0FR6_PWM			     =	753,
+
+            // H26R0x (Load-Cell Module)
+            CODE_H26R0_STREAM_PORT_GRAM  = 1901,
             CODE_H26R0_STREAM_PORT_KGRAM = 1902,
             CODE_H26R0_STREAM_PORT_OUNCE = 1903,
             CODE_H26R0_STREAM_PORT_POUND = 1904,
-            CODE_H26R0_STOP = 1905,
-            CODE_H26R0_ZEROCAL = 1910,
+            CODE_H26R0_STOP              = 1905,
+            CODE_H26R0_ZEROCAL           = 1910,
         }
 
+        #region Options_Byte
+        // 8th bit (MSB): Long messages flag. If set, then message parameters continue in the next message.
+        public enum Options8_Next_Message
+        {
+            FALSE = 0,
+            TRUE = 1,
+        }
 
-        #region Options
+        // 6-7th bits:
+        public enum Options67_Response_Options
+        {
+            SEND_BACK_NO_RESPONSE = 00,
+            SEND_RESPONSES_ONLY_MESSAGES = 01,
+            SEND_RESPONSES_ONLY_TO_CLI_COMMANDS = 10,
+            SEND_RESPONSES_TO_EVERYTHING = 11,
+        }
 
-        //// 8th bit (MSB): Long messages flag. If set, then message parameters continue in the next message.
-        //public enum Options8_Next_Message
-        //{
-        //    FALSE = 0,
-        //    TRUE = 1,
-        //}
+        // 5th bit: reserved.
+        public enum Options5_Reserved
+        {
+            FALSE = 0,
+            TRUE = 1,
+        }
 
-        //// 6-7th bits:
-        //public enum Options67_Response_Options
-        //{
-        //    SEND_BACK_NO_RESPONSE = 00,
-        //    SEND_RESPONSES_ONLY_MESSAGES = 01,
-        //    SEND_RESPONSES_ONLY_TO_CLI_COMMANDS = 10,
-        //    SEND_RESPONSES_TO_EVERYTHING = 11,
-        //}
+        // 3rd-4th bits:
+        public enum Options34_Trace_Options
+        {
+            SHOW_NO_TRACE = 00,
+            SHOW_MESSAGE_TRACE = 01,
+            SHOW_MESSAGE_RESPONSE_TRACE = 10,
+            SHOW_TRACE_FOR_BOTH_MESSAGES_AND_THEIR_RESPONSES = 11,
+        }
 
-        //// 5th bit: reserved.
-        //public enum Options5_Reserved
-        //{
-        //    FALSE = 0,
-        //    TRUE = 1,
-        //}
+        // 2nd bit: Extended Message Code flag. If set, then message codes are 16 bits.
+        public enum Options2_16_BIT_Code
+        {
+            FALSE = 0,
+            TRUE = 1,
+        }
 
-        //// 3rd-4th bits:
-        //public enum Options34_Trace_Options
-        //{
-        //    SHOW_NO_TRACE = 00,
-        //    SHOW_MESSAGE_TRACE = 01,
-        //    SHOW_MESSAGE_RESPONSE_TRACE = 10,
-        //    SHOW_TRACE_FOR_BOTH_MESSAGES_AND_THEIR_RESPONSES = 11,
-        //}
-
-        //// 2nd bit: Extended Message Code flag. If set, then message codes are 16 bits.
-        //public enum Options2_16_BIT_Code
-        //{
-        //    FALSE = 0,
-        //    TRUE = 1,
-        //}
-
-        //// 1st bit (LSB): Extended Options flag. If set, then the next byte is an Options byte as well.
-        //public enum Options1_Extended_Flag
-        //{
-        //    FALSE = 0,
-        //    TRUE = 1,
-        //}
-
+        // 1st bit (LSB): Extended Options flag. If set, then the next byte is an Options byte as well.
+        public enum Options1_Extended_Flag
+        {
+            FALSE = 0,
+            TRUE = 1,
+        }
         #endregion
     }
 }
